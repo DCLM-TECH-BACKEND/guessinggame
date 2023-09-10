c37267c51ffd2238bfac6cb9943d0d04ef51b9d9
Josh Ajiniran <josuajiniran@gmail.com>
2023-09-10 22:44:58 +0100

    create game session class

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ class Player:
     username: str
     ranking: int = 0
     games_won: int = 0
+    
+    
+@dataclass
+class GameSession:
+    player_1: Player
+    player_2: Player
+    scores: dict
+    
+    def create_session(self):
+        pass
+
 
 
 def create_player():
